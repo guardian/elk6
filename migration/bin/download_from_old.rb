e22eea700f3ef05354084627abd51c89df2662ec
Andy Gallagher <andy.gallagher@theguardian.com>
2018-06-08 10:56:54 +0100

    adding retries on 503/504 and an option to skip already downloaded chunks

--- migration/bin/download_from_old.rb
+++ migration/bin/download_from_old.rb
@@ -15,6 +15,7 @@ opts = Trollop::options do
   opt :output, "Output json files to this directory", :type=>:string,:default=>Dir.pwd
   opt :chunksize, "Get this many records per json file", :type=>:integer, :default=>1000
   opt :sortfield, "Sort by this field when retrieving", :type=>:string, :default=>"timestamp"
+  opt :redownload, "Overwrite existing files rather than skip them", :type=>:boolean, :default=>false
 end
 
 def check_cluster(address)
@@ -39,24 +40,39 @@ def check_index(address, indexname)
   content
 end
 
-def download_next_page(http,address,indexname, outputdir, page_number, page_size, sortfield)
+def download_next_page(http,address,indexname, outputdir, page_number, page_size, sortfield, redownload)
   #returns TRUE if more results, otherwise FALSE
   from = page_number * page_size
-  uri = URI("#{address}/#{indexname}/_search?from=#{from}&size=#{page_size}")
-  $logger.debug("Connecting to #{uri}...")
-  request = Net::HTTP::Post.new(uri)
-  request['Content-Type'] = 'application/json'
-  
-  request.body = JSON.generate({"query":{"match_all": {}}, "sort":[{"#{sortfield}":"asc"}]})
-  response = http.request request
+  outfile = File.join(outputdir,"indexdata-#{indexname}-#{page_number}.json")
 
-  if response.code != '200'
-    $logger.error(response.body)
-    raise RuntimeError, "Could not download page data, server returned #{response.code} error"
+  if File.exists?(outfile) and not redownload
+    $logger.info("File #{outfile} already exists, skipping download of this chunk")
+    return
   end
 
+  uri = URI("#{address}/#{indexname}/_search?from=#{from}&size=#{page_size}")
+  $logger.debug("Connecting to #{uri}...")
+
+  while true
+    request = Net::HTTP::Post.new(uri)
+    request['Content-Type'] = 'application/json'
+
+    request.body = JSON.generate({"query":{"match_all": {}}, "sort":[{"#{sortfield}":"asc"}]})
+    response = http.request request
+
+    if response.code == '504' or response.code=='503'
+      $logger.warning("Got timeout error #{response.code} from server, retrying in 10s")
+      sleep(10)
+    elsif response.code != '200'
+      $logger.error(response.body)
+      raise RuntimeError, "Could not download page data, server returned #{response.code} error"
+    else
+      break
+    end
+  end #while true
+
   content = JSON.parse(response.body)
-  outfile = File.join(outputdir,"indexdata-#{indexname}-#{page_number}.json")
+
   expected_pages = content['hits']['total']/page_size
   $logger.info("Writing page #{page_number} (out of expected #{expected_pages}) to #{outfile}")
   open outfile,"w" do |f|
@@ -77,10 +93,14 @@ indexinfo = check_index(opts.cluster, opts[:index])
 ap indexinfo
 
 outpath = File.join(opts.output,"indexinfo-#{opts[:index]}.json")
-$logger.info("Outputting to #{outpath}")
-open outpath, "w" do |f|
-  f.write(JSON.generate(indexinfo))
-end #open
+if File.exists?(outpath) and not opts.redownload
+  $logger.info("Not overwriting #{outpath}")
+else
+  $logger.info("Outputting to #{outpath}")
+  open outpath, "w" do |f|
+    f.write(JSON.generate(indexinfo))
+  end #open
+end #if File.exists?
 
 page_number=0
 
@@ -89,7 +109,7 @@ search_uri = URI("#{opts.cluster}/#{opts[:index]}/_search")
 Net::HTTP.start(search_uri.host, search_uri.port) do |http|
   more_hits=true
   while more_hits
-    more_hits=download_next_page(http, opts.cluster, opts[:index], opts.output, page_number, opts.chunksize, opts.sortfield)
+    more_hits=download_next_page(http, opts.cluster, opts[:index], opts.output, page_number, opts.chunksize, opts.sortfield, opts.redownload)
     ap more_hits
     page_number+=1
   end #while True
